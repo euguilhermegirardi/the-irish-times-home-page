77575bb7e3921b7b6549a50c01e4cb63a056987d
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-01 17:16:57 +0000

    slider working...

--- src/js/slider.js
+++ src/js/slider.js
@@ -10,18 +10,57 @@ function Slider(slider) {
 
   // select the elements needed for the slider
   const slides = slider.querySelector('.slider__slides');
-  const prevButton = slider.querySelector('.controls__goToPrev');
-  const nextButton = slider.querySelector('.controls__goToNext');
-}
+  const prevButton = slider.querySelector('.controls__prev');
+  const nextButton = slider.querySelector('.controls__next');
 
-function startSlider() {
-  current = slider.querySelector('.current') || slides.firstElementChild;
-  prev = current.previousElementSibling || slides.lastElementChild;
-  next = current.nextElementSibling || slides.firstElementChild;
-  console.log({ current, prev, next });
+  function startSlider() {
+    current = slider.querySelector('.current') || slides.firstElementChild;
+    prev = current.previousElementSibling || slides.lastElementChild;
+    next = current.nextElementSibling || slides.firstElementChild;
+    console.log({ current, prev, next });
+  }
+
+  function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next')
+  }
+
+  function move(direction) {
+    // first strip all the classes off the current slides
+    const classesToRemove = ['prev', 'current', 'next'];
+
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+
+    if (direction === 'back') {
+      // make an new array of new values and destructure them over and into the prev, current and next variables
+      [prev, current, next] = [
+        // get the prev slide, if there is none, get the last slide from the entire slider for wrapping
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
+    } else {
+      [prev, current, next] = [
+        current,
+        next,
+        // get the next slide, or if it's at the end, loop around and grab the first slide
+        next.nextElementSibling || slides.firstElementChild,
+      ];
+    }
+
+    applyClasses();
+  }
+
+  startSlider();
+  applyClasses();
+
+  // Event Listener
+  prevButton.addEventListener('click', () => move('back'));
+  nextButton.addEventListener('click', move);
 }
 
 const mySlider = Slider(document.querySelector('.slider'));
-
-console.log('POOOORA!');
 
