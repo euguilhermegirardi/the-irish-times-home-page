cd5964d919b5016c6b154f120cf3aeb816ddac14
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-16 19:16:24 +0000

    search dropdown

---
+++ src/js/searchDropdown.js
@@ -0,0 +1,4 @@
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+function searchDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
