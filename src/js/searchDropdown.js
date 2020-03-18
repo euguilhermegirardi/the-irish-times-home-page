29b9b957ef4e6fdeef67af477693797a113ed817
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-18 12:43:12 +0000

    close search bar

--- src/js/searchDropdown.js
+++ src/js/searchDropdown.js
@@ -1,4 +1,28 @@
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+// OPEN AND CLOSE SEARCH
 function searchDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+// OPEN AND CLOSE SEARCH ENDS!
+
+
+// Close search bar clicking outside.
+const modalOuter = document.querySelector('.navbar__searchDropdown');
+
+function closeSearch() {
+  modalOuter.classList.remove('show');
+}
+
+modalOuter.addEventListener('click', function(e) {
+  const isOutside = !e.target.closest('.navbar__search-pushdown');
+  if(isOutside) {
+    closeSearch();
+  }
+});
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeSearch();
+  }
+});
+
+// Close search bar clicking outside. ENDS!
