f15f438e5a4492681f85bef778a4b762e62db235
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-18 16:26:10 +0000

    all item dropdown

---
+++ src/js/allDropdown.js
@@ -0,0 +1,6 @@
+// OPEN AND CLOSE ALL ITEM
+function allDropdown() {
+  console.log('clicked');
+  document.getElementById("allDropdown").classList.toggle("show2");
+}
+// OPEN AND CLOSE ALL ITEM ENDS!
