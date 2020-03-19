5983fc4d0851954e2ac28efeb919ce160e6b5294
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-19 20:05:59 +0000

    svg ready / all dropdown (menu) svg ready

--- src/js/allDropdown.js
+++ src/js/allDropdown.js
@@ -1,12 +1,8 @@
 // OPEN AND CLOSE ALL ITEM
 function allDropdown() {
-  document.getElementById("allDropdown").classList.toggle("show2");
+  document.querySelector("#allDropdown").classList.toggle("show2");
 
-  // const x = document.getElementById("allItem");
-  // if (x.innerHTML === "All Business") {
-  //   x.innerHTML = "All Business sdasdad";
-  // } else {
-  //   x.innerHTML = "All Business &uArr;";
-  // }
+  const svg = document.querySelector('.menu__info-svg');
+  svg.classList.toggle('menu__info-svg--open');
 }
 // OPEN AND CLOSE ALL ITEM ENDS!
