e80b9d7c3c3c3fb549378863ab4058bad0415e36
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-22 14:46:51 +0000

    review of BEM structure

--- src/js/scrollNavbar.js
+++ src/js/scrollNavbar.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
-    document.querySelector("#scrollNavbar").style.top = "0";
+    document.querySelector("#navbar--scroll").style.top = "0";
   } else {
-    document.querySelector("#scrollNavbar").style.top = "-65px";
+    document.querySelector("#navbar--scroll").style.top = "-65px";
   }
 }
--- src/js/searchDropdown.js
+++ src/js/searchDropdown.js
@@ -3,7 +3,7 @@ const search = document.querySelector('.navbar__item');
 
 function searchDropdown() {
   search.addEventListener('click', () => {
-    document.querySelector("#myDropdown").classList.toggle("show");
+    document.querySelector("#navbar__search-dropdown").classList.toggle("show");
   })
 }
 searchDropdown();
@@ -11,14 +11,14 @@ searchDropdown();
 
 
 // CLOSE SEARCH BY CLICKING OUTSIDE
-const modalOuter = document.querySelector('.navbar__searchDropdown');
+const modalOuter = document.querySelector('.navbar__search');
 
 function closeSearch() {
   modalOuter.classList.remove('show');
 }
 
 modalOuter.addEventListener('click', function(e) {
-  const isOutside = !e.target.closest('.navbar__search-pushdown');
+  const isOutside = !e.target.closest('.navbar__pushdown');
   if(isOutside) {
     closeSearch();
   }
