22a96be0910c12d92bcd365d95611acc25783598
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-22 16:04:39 +0000

    BEM structure review

--- src/js/allDropdown.js
+++ src/js/allDropdown.js
@@ -2,10 +2,10 @@
 function allDropdown() {
   document.querySelector("#allDropdown").classList.toggle("show2");
 
-  const svg = document.querySelector('.menu__info-svg');
-  svg.classList.toggle('menu__info-svg--open');
+  const svg = document.querySelector('.menu__item-svg');
+  svg.classList.toggle('menu__item-svg-open');
 
-  const active = document.querySelector('#menu__info-all');
+  const active = document.querySelector('.menu__item--info-all');
   active.classList.toggle('active');
 }
 // OPEN AND CLOSE ALL ITEM ENDS!
