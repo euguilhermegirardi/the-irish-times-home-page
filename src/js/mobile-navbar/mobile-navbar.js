7f1e46549cab66c005cb7a3623fa66688147b46f
Guilherme Girardi <girardi.gui@icloud.com>
2020-03-31 17:19:03 +0100

    hamburger menu done

--- src/js/mobile-navbar/mobile-navbar.js
+++ src/js/mobile-navbar/mobile-navbar.js
@@ -1,10 +1,16 @@
-const icon = document.querySelector('.nav-sd__icon');
+const icon = document.querySelector('.nav-sd__icon-container');
 const menu = document.querySelector('.nav-sd__menu');
+const iconTop = document.querySelector('.icon-top');
+const iconMiddle = document.querySelector('.icon-middle');
+const iconBottom = document.querySelector('.icon-bottom');
 
 icon.addEventListener('click', openMenu);
 
 function openMenu() {
   menu.classList.toggle('nav-sd__open');
+  iconTop.classList.toggle('top-opened');
+  iconMiddle.classList.toggle('middle-opened');
+  iconBottom.classList.toggle('bottom-opened');
 }
 
 window.addEventListener('keydown', event => {
