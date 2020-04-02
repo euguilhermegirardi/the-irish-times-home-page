0df719d385a77b92842251cb18219678f58ff2ca
Guilherme Girardi <girardi.gui@icloud.com>
2020-04-02 20:40:57 +0100

    navigation small devices

--- src/js/mobile-navbar/mobile-navbar.js
+++ src/js/mobile-navbar/mobile-navbar.js
@@ -1,16 +1,13 @@
 const icon = document.querySelector('.nav-sd__icon-container');
 const menu = document.querySelector('.nav-sd__menu');
-
-const iconList = document.querySelector('.nav-sd__close-icon-container');
-const openedIcon = iconList.querySelectorAll('.nav-sd__close-icon');
-
-const close = document.querySelector('.nav-sd__close-icon-container');
+const close = document.querySelector('.icon-container');
 
 // OPEN MENU
 icon.addEventListener('click', openMenu);
+
 function openMenu() {
   menu.classList.add('nav-sd__open');
-}
+};
 
 // CLOSE MENU
 close.addEventListener('click', function closeMenu() {
