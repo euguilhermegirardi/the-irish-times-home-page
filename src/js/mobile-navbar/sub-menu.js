d6fb796dc9a57a9a603a01861a23c588b60a6ec8
Guilherme Girardi <girardi.gui@icloud.com>
2020-04-03 14:00:08 +0100

    accordion on navigation small devices

--- src/js/mobile-navbar/sub-menu.js
+++ src/js/mobile-navbar/sub-menu.js
@@ -1,18 +1,64 @@
-const dropdown = document.getElementsByClassName('nav-sd__dropdown');
-var i;
-
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-
-  this.classList.toggle("nav-sd__active");
-
-  const dropdownContent = this.nextElementSibling;
-
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
-  });
+// ACCORDION
+const toggleItem = (item) => {
+  if (isItemOpen(item))
+  closeItem(item);
+  else
+    openItem(item);
 }
+
+const isItemOpen = (item) => {
+  return item.getAttribute('data-state') === 'open';
+}
+
+const closeItem = (item) => {
+  item.setAttribute("data-state", "closed");
+  item.classList.remove('nav-sd__active');
+
+  var dropdown = item.nextElementSibling;
+  if(dropdown) {
+    dropdown.style.maxHeight = null;
+  }
+  // var icon = tab.firstElementChild;
+  // if(icon) {
+  //   icon.innerHTML = '';
+  // }
+}
+
+const openItem = (item) => {
+  item.setAttribute('data-state', 'open');
+  item.classList.add('nav-sd__active');
+
+  var dropdown = item.nextElementSibling;
+  if(dropdown) {
+    dropdown.style.maxHeight = dropdown.scrollHeight + 'px';
+  }
+
+  // var icon = dropdown.firstElemtentChild;
+  // if(icon) {
+  //   icon.innerHTML = 'X';
+  // }
+}
+
+const closeOtherItems = (item) => {
+  var items = document.getElementsByClassName('nav-sd__list-item');
+  for (var i = 0; i < items.length; i++) {
+    let otherItem = items[i];
+    if(isItemOpen(otherItem) && otherItem !== item)
+    closeItem(otherItem);
+  }
+};
+
+const initItems = () => {
+  var items = document.getElementsByClassName('nav-sd__list-item');
+  for (var i = 0; i < items.length; i++) {
+    let item = items[i];
+    closeItem(item);
+
+    item.addEventListener('click', () => {
+      closeOtherItems(item);
+      toggleItem(item);
+    });
+  }
+}
+
+initItems();
